194a0de1a177f517360bce09db2ffbe02ecbd660
ThiagoPSs <79084088+ThiagoPSs@users.noreply.github.com>
2021-03-12 21:35:42 -0300

    Update painel.js

--- javascript/painel.js
+++ javascript/painel.js
@@ -15,8 +15,8 @@ class Painel{
     validar(p){
         return p.shape.every((row, dy) => {
             return row.every((value, dx) => {
-                    let x = p.x + dx;
-                    let y = p.y + dy;
+                  let x = p.x + dx;
+                   let y = p.y + dy;
                     return(
                     this.grid[y] && this.grid[y][x] === 0 || (x >= 0 && x < COLUNA && y <= LINHA) 
                 );
